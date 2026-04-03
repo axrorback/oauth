e77a918565efc6d7288d53fec1b798bca1d081f6
axrorback <axrorback@gmail.com>
2026-04-03 22:37:19 +0500

    Wrap profile creation in a transaction.on_commit hook to ensure database consistency.

--- users/signals.py
+++ users/signals.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User, Profile
@@ -5,4 +6,6 @@ from .models import User, Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        transaction.on_commit(
+            lambda: Profile.objects.get_or_create(user=instance)
+        )
