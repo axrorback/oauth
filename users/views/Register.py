9993ec713feb49f6dd9de5206d872a15119de09b
axrorback <axrorback@gmail.com>
2026-04-03 22:36:57 +0500

    Refactor RegisterView to use FormView, add email verification workflow with token generation and async email task

--- users/views/Register.py
+++ users/views/Register.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render ,redirect
-from django.views import View
+from django.shortcuts import render ,redirect , reverse
+from django.views.generic import FormView
+from users.forms import RegisterForm
 from users.service import TokenService
-from django.contrib.auth import authenticate, login
+from users.models import EmailVerificationToken
+from users.tasks import EmailTask
 
-
-class RegisterView(View):
+class RegisterView(FormView):
     template_name = 'users/register.html'
-    def get(self, request):
-        return render(request, self.template_name)
-    def post(self, request):
-        pass
+    form_class = RegisterForm
+    success_url = '/check-email/'
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        raw_token = TokenService.registration_token()
+        hashed_token = TokenService.hash(raw_token)
+
+        EmailVerificationToken.objects.create(
+            user=user,
+            token=hashed_token
+        )
+
+        verification_link = self.request.build_absolute_uri(reverse('verify')) + f'?token={raw_token}'
+        EmailTask.delay(user.email, verification_link)
+        print(verification_link)
+        return super().form_valid(form)
