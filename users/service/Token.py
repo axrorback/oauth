f1995df06be9a2724ad15f99ca762131b6dc1677
axrorback <axrorback@gmail.com>
2026-04-03 22:37:26 +0500

    Add hashing functionality to TokenService for secure token storage

--- users/service/Token.py
+++ users/service/Token.py
@@ -1,3 +1,4 @@
+import hashlib
 from uuid import uuid4
 
 class TokenService:
@@ -8,6 +9,7 @@ class TokenService:
     @staticmethod
     def reset_token():
         return str(uuid4())
+
     @staticmethod
     def hash(token: str) -> str:
         return hashlib.sha256(token.encode()).hexdigest()
