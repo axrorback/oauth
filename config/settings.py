ba69f9d8339bd4ecbb001fe30901d273b744f16e
axrorback <axrorback@gmail.com>
2026-05-09 20:13:50 +0500

    Clean up settings.py by removing obsolete comments and ensuring environment variable usage for SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -4,14 +4,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
