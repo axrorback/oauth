aef7e83f19b133606a1c11a23a63888b2c0ff028
axrorback <axrorback@gmail.com>
2026-05-10 16:43:35 +0500

    Remove unused `CustomAuthorizeView` route from `urls.py` to clean up routing configuration

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.urls import path , include
 from users.views import CustomAuthorizeView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('o/authorize/',CustomAuthorizeView.as_view(),name='oauth_authorize'),
     path('users/',include('users.urls')),
     path('o/',include('oauth2_provider.urls',namespace='oauth2_provider')),
     path('',include('home.urls')),
