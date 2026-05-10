5f98d8c939812ff584977956c49de93e5f188db9
axrorback <axrorback@gmail.com>
2026-05-10 16:12:56 +0500

    Add `bio` field to `Profile` model for extended user profile customization

--- users/models/Profile.py
+++ users/models/Profile.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 import uuid
 from users.models import User
 
@@ -7,6 +6,6 @@ class Profile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     avatar = models.ImageField(upload_to="oauth/images/", blank=True, null=True)
-
+    bio = models.CharField(max_length=50, blank=True,null=True)
     def __str__(self):
         return f"{self.user} profile"
