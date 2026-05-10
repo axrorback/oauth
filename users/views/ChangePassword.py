9bebc160b72f277efae05694fa259ad9ff683c7f
axrorback <axrorback@gmail.com>
2026-05-10 16:14:00 +0500

    Add reCAPTCHA v3 to `ChangePasswordView` for enhanced security

--- users/views/ChangePassword.py
+++ users/views/ChangePassword.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from users.mixins import ReCaptchaV3Mixin
 
-
-class ChangePasswordView(LoginRequiredMixin,PasswordChangeView):
+class ChangePasswordView(LoginRequiredMixin,ReCaptchaV3Mixin,PasswordChangeView):
     template_name = 'users/change_password.html'
     success_url = reverse_lazy('change_password_done')
