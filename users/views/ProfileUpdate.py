657b101b3f1850b8eb0ea8d90153eb7ad83c2d5e
axrorback <axrorback@gmail.com>
2026-05-10 16:12:43 +0500

    Add reCAPTCHA v3 and refactor `ProfileUpdateView` to enhance security and code structure

--- users/views/ProfileUpdate.py
+++ users/views/ProfileUpdate.py
@@ -1,19 +1,41 @@
-from django.views.generic import TemplateView
+from django.views import View
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
+from django.contrib import messages
 from users.forms import UserUpdateForm, ProfileUpdateForm
+from users.mixins import ReCaptchaV3Mixin
+from django.conf import settings
 
+class ProfileUpdateView(LoginRequiredMixin, ReCaptchaV3Mixin, View):
 
-class ProfileUpdateView(LoginRequiredMixin, TemplateView):
+    recaptcha_action = "submit"
+    recaptcha_min_score = 0.5
     template_name = "users/profile_edit.html"
 
-    def get(self, request, *args, **kwargs):
-        return self.render_to_response({
+    def get_context(self, request):
+
+        return {
             "user_form": UserUpdateForm(instance=request.user),
-            "profile_form": ProfileUpdateForm(instance=request.user.profile)
-        })
+            "profile_form": ProfileUpdateForm(instance=request.user.profile),
+            "SITE_KEY": settings.RECAPTCHA_PUBLIC_KEY,
+            "recaptcha_action": self.recaptcha_action,
+        }
+
+    def get(self, request, *args, **kwargs):
+
+        return render(request, self.template_name, self.get_context(request))
 
     def post(self, request, *args, **kwargs):
+
+        if not self.verify_recaptcha():
+            messages.error(request, "Captcha xato")
+
+            return render(
+                request,
+                self.template_name,
+                self.get_context(request)
+            )
+
         user_form = UserUpdateForm(request.POST, instance=request.user)
         profile_form = ProfileUpdateForm(
             request.POST,
@@ -26,7 +48,8 @@ class ProfileUpdateView(LoginRequiredMixin, TemplateView):
             profile_form.save()
             return redirect("profile")
 
-        return self.render_to_response({
+        return render(request, self.template_name, {
+            **self.get_context(request),
             "user_form": user_form,
-            "profile_form": profile_form
+            "profile_form": profile_form,
         })
