15be4811ee2d11ccd861c2e7af7d6d8c39fdda4b
axrorback <axrorback@gmail.com>
2026-05-10 22:03:18 +0500

    Update `authorize.html`: Change default application name fallback to 'Nomalum'

--- users/tasks.py
+++ users/tasks.py
@@ -1,22 +1,26 @@
-# users/tasks.py
 from celery import shared_task
-from django.core.mail import send_mail
 from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-from django.conf import settings
+from users.brevo_sender import send_brevo_email
+
 
 @shared_task(name="send_verification_email")
 def send_verification_email(email, verify_url):
-    subject = 'CODERBOYS OAuth2.0 Server Verification'
-    context = {'verify_url': verify_url}
-    html_message = render_to_string('emails/verify.html', context)
-    plain_message = strip_tags(html_message)
 
-    send_mail(
-        subject=subject,
-        message=plain_message,
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[email],
-        html_message=html_message
+    subject = "Emailni Tasdiqlang !"
+
+    context = {
+        "verify_url": verify_url
+    }
+
+    html_message = render_to_string(
+        "emails/verify.html",
+        context
     )
-    return f'Email sent to {email}'
+
+    send_brevo_email(
+        to_email=email,
+        subject=subject,
+        html_content=html_message,
+    )
+
+    return f"Verification email sent to {email}"
